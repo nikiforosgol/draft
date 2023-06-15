051c14ff9c4cc484461dfe001bddb41866d63523
nikiforosgol <nikiforosgol@gmail.com>
2023-06-15 14:24:26 +0300

    basic working

--- users/admin.py
+++ users/admin.py
@@ -4,16 +4,26 @@ from .models import *
 
 
 class DocumentInline(admin.TabularInline):
-    model = Document
+    model = Document.project.through
+    extra = 1
+
+
+class PlaceholderInline(admin.TabularInline):
+    model = Placeholder.documents.through
+    extra = 1
 
 
 class ProjectAdmin(admin.ModelAdmin):
     inlines = [DocumentInline]
 
 
+class DocumentAdmin(admin.ModelAdmin):
+    inlines = [PlaceholderInline]
+
+
 admin.site.register(PracticeArea)
-admin.site.register(Project)
-admin.site.register(Document)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Document, DocumentAdmin)
 admin.site.register(Placeholder)
 
 
